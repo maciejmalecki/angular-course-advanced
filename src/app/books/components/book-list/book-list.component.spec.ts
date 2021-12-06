fe431f2c1c9a1aa2907849e44e45e25ab11fa520
Maciej Malecki <maciej.malecki@capgemini.com>
2021-12-06 09:24:52 +0100

    DOM tests of list component

--- src/app/books/components/book-list/book-list.component.spec.ts
+++ src/app/books/components/book-list/book-list.component.spec.ts
@@ -1,9 +1,11 @@
-import { BookListComponent } from './book-list.component';
+import {BookListComponent} from './book-list.component';
 import {BookService} from "../../services/book.service";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
 
 describe('BookListComponent', () => {
+  let component: BookListComponent;
+
   describe("[class]", () => {
-    let component: BookListComponent;
     let bookService: BookService;
 
     beforeEach(() => {
@@ -36,6 +38,98 @@ describe('BookListComponent', () => {
   });
 
   describe("[DOM]", () => {
+
+    let fixture: ComponentFixture<BookListComponent>;
+    let nativeElement: HTMLElement;
+    let bookService: BookService;
+
+    const bookList = () => nativeElement.querySelectorAll('ul.list-group.list-group-numbered li');
+    const bookAt = (position: number) => bookList().item(position);
+    const clickBookAt = (position: number) => bookAt(position).dispatchEvent(new MouseEvent('click'));
+    const editor = () => nativeElement.querySelector(".editor");
+    const cancelButton = () => nativeElement.querySelector("button.btn.btn-light")!;
+    const clickCancelButton = () => cancelButton().dispatchEvent(new MouseEvent('click'));
+    const saveButton = () => nativeElement.querySelector("button.btn.btn-primary")!;
+    const clickSaveButton = () => saveButton().dispatchEvent(new MouseEvent('click'));
+    const title = () => nativeElement.querySelector("input#title")! as HTMLInputElement;
+    const author = () => nativeElement.querySelector("input#author")! as HTMLInputElement;
+    const description = () => nativeElement.querySelector("textarea#description")! as HTMLTextAreaElement;
+    const editField = (field: HTMLInputElement | HTMLTextAreaElement, value: string) => {
+      field.value = value;
+    };
+
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [BookListComponent],
+        providers: [BookService]
+      }).compileComponents();
+    });
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(BookListComponent);
+      component = fixture.componentInstance;
+      nativeElement = fixture.nativeElement;
+      bookService = TestBed.inject(BookService);
+      fixture.detectChanges();
+    });
+
+    it("can be created", () => {
+      expect(component).toBeTruthy();
+    });
+
+    it("renders list of books", () => {
+      const liElements: NodeList = bookList();
+      expect(liElements.length).toBe(3);
+    });
+
+    it('selects book on clicking', () => {
+      // given
+      expect(component.selectedBook).toBeFalsy();
+      expect(editor()).toBeFalsy();
+      // when
+      clickBookAt(1);
+      fixture.detectChanges();
+      // then
+      expect(component.selectedBook).toBeTruthy();
+      expect(component.selectedBook).toEqual(component.books[1]);
+      expect(editor()).toBeTruthy();
+    });
+
+    it('closes book on clicking cancel', () => {
+      // given
+      clickBookAt(1);
+      fixture.detectChanges();
+      expect(editor()).toBeTruthy();
+      // when
+      clickCancelButton();
+      fixture.detectChanges();
+      // then
+      expect(component.selectedBook).toBeFalsy();
+      expect(editor()).toBeFalsy();
+    });
+
+    it('saves modified book to the service', () => {
+      // given
+      spyOn(bookService, "saveBook").and.callThrough();
+      clickBookAt(1);
+      fixture.detectChanges();
+      expect(editor()).toBeTruthy();
+      // when
+      editField(title(), "foo");
+      editField(author(), "bar");
+      editField(description(), "some description");
+      clickSaveButton();
+      fixture.detectChanges();
+      // then
+      expect(editor()).toBeFalsy();
+      expect(component.selectedBook).toBeFalsy();
+      expect(bookService.saveBook).toHaveBeenCalledOnceWith({
+        id: 2,
+        title: "foo",
+        author: "bar",
+        description: "some description"
+      });
+    });
 
   });
 });
