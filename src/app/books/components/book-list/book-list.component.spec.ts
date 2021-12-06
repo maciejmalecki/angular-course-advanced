6ec9f048a9b52355833e660590f8fbadb89b93c4
Maciej Malecki <maciej.malecki@capgemini.com>
2021-12-06 08:43:43 +0100

    class tests for list component

--- src/app/books/components/book-list/book-list.component.spec.ts
+++ src/app/books/components/book-list/book-list.component.spec.ts
@@ -1,25 +1,41 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BookListComponent } from './book-list.component';
+import {BookService} from "../../services/book.service";
 
 describe('BookListComponent', () => {
-  let component: BookListComponent;
-  let fixture: ComponentFixture<BookListComponent>;
+  describe("[class]", () => {
+    let component: BookListComponent;
+    let bookService: BookService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BookListComponent ]
-    })
-    .compileComponents();
+    beforeEach(() => {
+      bookService = new BookService();
+      component = new BookListComponent(bookService);
+    });
+
+    it("fetches books during construction", () => {
+      expect(component.books).toHaveSize(3);
+    });
+
+    it("allows selecting book by id", () => {
+      // when
+      const book = component.books[1];
+      component.selectBook(book.id);
+      // then
+      expect(component.selectedBook).toEqual(book);
+    });
+
+    it("cancel deselect previously selected book", () => {
+      // given
+      const book = component.books[0];
+      component.selectBook(book.id);
+      expect(component.selectedBook).toBeTruthy();
+      // when
+      component.cancel();
+      // then
+      expect(component.selectedBook).toBeFalsy();
+    });
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BookListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  describe("[DOM]", () => {
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
   });
 });
