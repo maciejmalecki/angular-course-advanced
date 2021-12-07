2c46d20f0a52e2ade02df03f7df0228278a8a8d0
Maciej Malecki <maciej.malecki@capgemini.com>
2021-12-07 11:27:30 +0100

    rendering the list

--- src/app/books/component/book-list/book-list.component.ts
+++ src/app/books/component/book-list/book-list.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {Book} from "../../model/book";
+import {BooksService} from "../../service/books.service";
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
+  styleUrls: ['./book-list.component.scss'],
+  providers: [BooksService]
 })
 export class BookListComponent implements OnInit {
 
-  constructor() { }
+  books: Book[];
+
+  constructor(private readonly bookService: BooksService) {
+    this.books = this.bookService.getBooks();
+  }
 
   ngOnInit(): void {
   }
