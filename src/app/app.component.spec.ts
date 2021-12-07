fb9287e411dcb15f7c34e86986c0de9a9555c52a
Maciej Malecki <maciej.malecki@capgemini.com>
2021-12-07 10:46:09 +0100

    02 books module

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Angular Class demo app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('app app is running!');
+    expect(compiled.querySelector('#title')?.textContent).toContain('app Angular Class demo app is running!');
   });
 });
---
+++ src/app/books/books.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookListComponent } from './component/book-list/book-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    BookListComponent
+  ],
+  exports: [
+    BookListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BooksModule { }
