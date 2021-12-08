dc0397ae8f39d7414166ec4676e8eb2c8d61689b
Maciej Malecki <maciej.malecki@capgemini.com>
2021-12-08 21:26:06 +0100

    fix tests

--- src/app/books/services/books.service.spec.ts
+++ src/app/books/services/books.service.spec.ts
@@ -1,13 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BooksService } from './books.service';
+import {HttpClient} from "@angular/common/http";
 
 describe('BooksService', () => {
   let service: BooksService;
+  let httpClientMock: any;
 
   beforeEach(() => {
+    httpClientMock = {};
     TestBed.configureTestingModule({
-      providers: [BooksService]
+      providers: [BooksService, {provide: HttpClient, useValue: httpClientMock}]
     });
     service = TestBed.inject(BooksService);
   });
