607715ebff1a03214b6ad316749af73acfd2395a
Maciej Malecki <maciej.malecki@capgemini.com>
2021-12-09 15:08:50 +0100

    Redux

---
+++ src/app/books/store/books.reducer.ts
@@ -0,0 +1,13 @@
+import {Book} from "../model/book";
+
+export interface BooksState {
+  books: Book[],
+  selectedBook: Book | null
+}
+
+export const initialBooksState: BooksState = {
+  books: [],
+  selectedBook: null
+};
+
+
