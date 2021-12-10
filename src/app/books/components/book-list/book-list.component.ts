86e1b0a6dc30f839b8359b3ac7d5245d88672ca3
Maciej Malecki <maciej.malecki@capgemini.com>
2021-12-10 13:12:37 +0100

    avoid selectedBookId

--- src/app/books/components/book-list/book-list.component.ts
+++ src/app/books/components/book-list/book-list.component.ts
@@ -1,15 +1,10 @@
 import {Component, OnDestroy} from '@angular/core';
 import {Book} from "../../model/book";
 import {Observable, Subject} from "rxjs";
-import {takeUntil} from "rxjs/operators";
+import {takeUntil, withLatestFrom} from "rxjs/operators";
 import {select, Store} from "@ngrx/store";
 import {BooksState} from "../../store/books.reducer";
-import {
-  deselectBookAction,
-  loadBooksAction,
-  saveBookAction,
-  selectBookAction,
-} from "../../store/books.actions";
+import {deselectBookAction, loadBooksAction, saveBookAction, selectBookAction,} from "../../store/books.actions";
 import {BooksSelector} from "../../store/books.selectors";
 
 @Component({
@@ -22,23 +17,27 @@ export class BookListComponent implements OnDestroy {
 
   readonly books$: Observable<Book[]>;
   readonly selectedBook$: Observable<Book | null>;
-  selectedBookId: number | undefined = undefined;
 
   private readonly unsubscribe = new Subject();
+  private readonly selectBookClicked$ = new Subject<Book>();
 
   constructor(private readonly store: Store<BooksState>) {
     this.store.dispatch(loadBooksAction());
     this.books$ = this.store.pipe(select(BooksSelector.getBooks));
     this.selectedBook$ = this.store.pipe(select(BooksSelector.getSelectedBook));
-    this.selectedBook$
-      .pipe(takeUntil(this.unsubscribe))
-      .subscribe(book => {
-        if(book) {
-          this.selectedBookId = book.id;
-        } else {
-          this.selectedBookId = undefined;
+    this.selectBookClicked$
+      .pipe(
+        takeUntil(this.unsubscribe),
+        withLatestFrom(this.selectedBook$))
+      .subscribe(
+        ([clickedBook, selectedBook]) => {
+          if (selectedBook && selectedBook.id === clickedBook.id) {
+            this.store.dispatch(deselectBookAction());
+          } else {
+            this.store.dispatch(selectBookAction({book: clickedBook}));
+          }
         }
-      });
+      );
   }
 
   ngOnDestroy(): void {
@@ -47,11 +46,7 @@ export class BookListComponent implements OnDestroy {
   }
 
   selectBook(book: Book): void {
-    if(this.selectedBookId === book.id) {
-      this.store.dispatch(deselectBookAction());
-    } else {
-      this.store.dispatch(selectBookAction({book}));
-    }
+    this.selectBookClicked$.next(book);
   }
 
   cancelEditing(): void {
@@ -59,7 +54,7 @@ export class BookListComponent implements OnDestroy {
   }
 
   saveBook(book: Book) {
-    this.store.dispatch(saveBookAction({ book }));
+    this.store.dispatch(saveBookAction({book}));
     this.store.dispatch(deselectBookAction());
   }
 }
